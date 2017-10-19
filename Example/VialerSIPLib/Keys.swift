780713eb15d4e52785f16f337aff22c2e45d8775
Antonijo <antonijo.bezmalinovic@globaldizajn.hr>
2017-10-19 09:50:28 +0200

    aa

---
+++ Example/VialerSIPLib/Keys.swift
@@ -0,0 +1,16 @@
+//
+//  Keys.swift
+//  Copyright © 2016 Devhouse Spindle. All rights reserved.
+//
+
+struct Keys {
+    struct SIP {
+        static let Account = "6d0aacbcab5677e6"
+        static let Password = "NHYsNKJVBnEpMK7a"
+        static let Domain = "ZT2F80B3ED18CA209A.sip.zipato.com"
+        static let Proxy = "10.10.10.50:5060"
+    }
+    static let NumberToCall = "6117186c0c2f39f4"
+    //static let NumberToCall = "19955db7e8611150"
+    
+}
